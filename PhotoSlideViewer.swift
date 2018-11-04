a4e3d9fad5b0d916c0a54acd69051ac2509e4871
Alex Bin Zhao <alex@popanything.com>
2018-11-04 19:22:10 +1100

    Fix swift 3 escaping change

--- PhotoSlideViewer.swift
+++ PhotoSlideViewer.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 @objc public protocol PhotoSlideViewerSource {
-    func loadPhoto(photoObject: AnyObject, titleCallBack: (_ title: String?, _ subtitle: String?) -> Void, videoCallBack: (_ isVideo: Bool) -> Void, imageCallBack: (_ image: UIImage?) -> Void)
+    func loadPhoto(photoObject: AnyObject, titleCallBack: @escaping (_ title: String?, _ subtitle: String?) -> Void, videoCallBack: @escaping (_ isVideo: Bool) -> Void, imageCallBack: @escaping (_ image: UIImage?) -> Void)
     func playVideo(photoObject: AnyObject)
 }
 
